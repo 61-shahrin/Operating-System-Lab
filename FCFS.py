909cba850f47be9ca9e84b69944276748759caef
Sadia Shahrin Sharna <shahrin999910@gmail.com>
2026-08-02 09:06:35 -0700

    FCFS File

---
+++ FCFS.py
@@ -0,0 +1,44 @@
+processes = [
+    ["P0", 3, 1],
+    ["P1", 5, 3],
+    ["P2", 2, 2],
+    ["P3", 1, 2],
+    ["P4", 6, 3]
+]
+
+
+processes.sort(key=lambda x: x[1])
+
+time = 0
+total_tat = 0
+total_wt = 0
+
+print("ID\tAT\tBT\tCT\tTAT\tWT")
+
+for p in processes:
+    p_id, at, bt = p
+
+
+    if time < at:
+        time = at
+
+    ct = time + bt
+    tat = ct - at
+    wt = tat - bt
+
+    total_tat += tat
+    total_wt += wt
+
+    print(f"{p_id}\t{at}\t{bt}\t{ct}\t{tat}\t{wt}")
+
+    time = ct
+
+n = len(processes)
+
+print("\nAvg TAT =", total_tat / n)
+print("Avg WT =", total_wt / n)
+
+print("\nExecution Sequence:")
+for p in processes:
+    print(p[0], end=" -> ")
+print("End")
